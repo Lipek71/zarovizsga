c9e6cfecaeabad6090c2375dbe7d5ffb0e5fdb72
Lipek <lipek71@gmail.com>
2021-04-28 18:16:50 +0200

    WorkHours javítás

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,5 +1,7 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class DigitsCounter {
@@ -7,18 +9,22 @@ public class DigitsCounter {
     public int getCountOfDigits(String s){
 
         final String NUMBERS = "0123456789";
-        TreeMap<String, Integer> map = new TreeMap();
-
+        //TreeMap<String, Integer> map = new TreeMap();
+        Set<Character> set = new HashSet<>();
         if (s == null || s.isEmpty()){
             return 0;
         }
 
         for (int i = 0; i < s.length(); i++){
-            if(NUMBERS.contains(s.substring(i,i+1))){
-                map.put(s.substring(i,i+1),0);
+            //if(NUMBERS.contains(s.substring(i,i+1))){
+            //    map.put(s.substring(i,i+1),0);
+            //}
+            if(Character.isDigit(s.charAt(i))){
+                set.add(s.charAt(i));
             }
         }
 
-        return map.size();
+        //return map.size();
+        return set.size();
     }
 }
